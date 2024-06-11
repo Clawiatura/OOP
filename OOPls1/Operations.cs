12bbc3c215fc47b407cf9684741dcc8fd5f554b2
Clawiatura <maksimov19992805@icloud.com>
2024-06-11 14:50:48 +0200

    OOP

---
+++ OOPls1/Operations.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace OOPls1
+{
+      class Operations
+    {
+
+        public List<string> values = new List<string>();
+
+        public void AddValue(string value)
+        {
+            values.Add(value);
+        }
+        public void Delete(int index)
+        {
+            if (index <= values.Count)
+            {
+                values.RemoveAt(index-1);
+            }
+        }
+        public int FindValue(string Values)
+        {
+            return values.IndexOf(Values);
+        }
+        public void Clear()
+        {
+            values.Clear();
+        }
+        public void Sort()
+        {
+            values.Sort();
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (string Value in values)
+            {
+                if (int.TryParse(Value, out int number))
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+        public int max()
+        {
+            int max = int.MinValue;
+            foreach (string Value in values)
+            {
+                if (int.TryParse(Value, out int number))
+                {
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                }
+            }
+            return max;
+
+        }
+        public int min()
+        {
+            int min = int.MaxValue;
+            foreach (string Value in values)
+            {
+                if (int.TryParse(Value, out int number))
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ OOPls1/Program.cs
@@ -0,0 +1,96 @@
+﻿using OOPls1;
+using System.Runtime.Serialization;
+using System.Security.Principal;
+Operations operations = new Operations();
+Console.WriteLine("Введите три значения:");
+for (int i = 0; i < 3; i++)
+    {
+    string value = Console.ReadLine();
+    operations.AddValue(value);
+}
+Console.WriteLine("Список");
+foreach (string value in operations.values)
+{
+    Console.WriteLine(value);
+}
+
+
+
+Console.WriteLine("Выберите действие:\\1. Удалить элемент\\2. Поиск значения\\3. Очистить список\\4. Сортировать список\\\\5. Вычислить сумму чисел\\6. Найти минимальное значение\\7. Найти максимальное значение");
+int number = int.Parse(Console.ReadLine()!);
+switch(number)
+{
+    case 1:
+        {
+            
+            Console.WriteLine("Введите индекс для удаления: ");
+            
+            int index = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n\n\n");
+            operations.Delete(index);
+            foreach (string value in operations.values)
+            {
+                Console.WriteLine(value);
+            }
+
+
+        }
+        break;
+    case 2: 
+        {
+            Console.WriteLine("Введите значение для поиска: ");
+            string Values = Console.ReadLine();
+            int index = operations.FindValue(Values);
+            if(index != -1)
+            {
+                Console.WriteLine($"Первое вхождение по индексу {index}") ;
+            }
+            else
+            {
+                Console.WriteLine("Введите другое значение");
+            }
+        }
+        break;
+    case 3: 
+        {
+            operations.Clear();
+            
+        }
+        break;
+    case 4:
+        {
+            operations.Sort();
+            foreach(string value in operations.values)
+            {
+                Console.WriteLine(value);
+            }
+            
+        }
+        break;
+    case 5:
+        {
+            int sum = operations.Sum();
+                Console.WriteLine($"{sum}");
+        }
+        break;
+    case 6:
+        {
+            int min = operations.min();
+            Console.WriteLine(min);
+        }
+        break;
+    case 7:
+        {
+            int max = operations.max();
+            Console.WriteLine(max);
+        }
+        break;
+
+        default:
+        {
+            Console.WriteLine("Неверный выбор");
+
+        }
+        break;
+
+}
