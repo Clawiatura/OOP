19fc90c4603381aa854916640f7f9339a680c603
Clawiatura <maksimov19992805@icloud.com>
2024-07-08 12:01:01 +0200

    x

---
+++ Операции над колекциями/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Операции_над_колекциями
+{
+    internal class Class
+    {
+        public string? TaskName { get; set; }
+       
+    }
+}
