f534cacdb6f48aafe058726c2c8d3bddaa14c2d8
Clawiatura <maksimov19992805@icloud.com>
2024-08-01 14:09:59 +0200

    s

---
+++ Delegate/Building.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Building
+    {
+        public string BuildingName { get; set; }
+        public double Height { get; set; }
+        public int Floors { get; set; }
+        public double TotalArea { get; set; }
+        public string Location { get; set; }
+
+        public double CalculateVolume()
+        {
+            return Height * TotalArea;
+        }
+        public delegate double VolumeCalculator(Building building);
+
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using Delegate;
+using static Delegate.Building;
+
+Building[] buildings = new Building[3];
+buildings[0] = new Building { BuildingName = "Sky Tower", Height = 150, Floors = 50, TotalArea = 2000, Location = "City Center" };
+buildings[1] = new Building { BuildingName = "Ocean View", Height = 80, Floors = 25, TotalArea = 1500, Location = "Coastal Area" };
+buildings[2] = new Building { BuildingName = "Mountain Lodge", Height = 30, Floors = 10, TotalArea = 500, Location = "Mountain Region" };
+
+CalculateBuildingVolumes(buildings, b => b.CalculateVolume());
+
+
+    static void CalculateBuildingVolumes(Building[] buildings, VolumeCalculator calc)
+{
+    foreach (var building in buildings)
+    {
+        Console.WriteLine($"Volume of {building.BuildingName}: {calc(building)} m³");
+    }
+}
