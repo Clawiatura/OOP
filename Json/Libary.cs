8762c00b1afce9365ce50bc7fbc8208357d2aea4
Clawiatura <maksimov19992805@icloud.com>
2024-08-06 14:43:28 +0200

    ы

---
+++ Json/Libary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    internal class Libary
+    {
+        private const string Filepath = "C:\\DZ\\users.json";
+        public List<Book> books = new List<Book>();
+        
+        
+        public async void SerializeToJson(string filePath, List<Book> books)
+        {
+            
+            
+            using (FileStream fs = new FileStream("C:\\DZ\\users.json", FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(fs, books);
+            }
+
+            
+            Console.WriteLine("Файл записан");
+
+        }
+
+        public async  void DeserializeToJson(string filePath)
+        {
+
+            using (FileStream fs = new FileStream(Filepath, FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    List<Book>? Book = await JsonSerializer.DeserializeAsync<List<Book>>(fs);
+                    books = Book;
+                }
+                catch { }
+            }
+
+
+
+        }
+        public string Print()
+        {
+            string res = " ";
+            foreach (Book us in books)
+            {
+                res += us.Author + " " + us.Title + " " + us.Genre + us.Year + "\n";
+            };
+            Console.WriteLine("считан");
+            return res;
+           
+            
+            
+        }
+    }
+}
---
+++ Json/Program.cs
@@ -0,0 +1,27 @@
+﻿using Json;
+using System.Xml.Linq;
+using static System.Reflection.Metadata.BlobBuilder;
+Libary libary = new Libary();
+
+
+List<Book> books = new List<Book>
+        {
+            new Book("zxc", "pussa", "asdsa", 1949),
+            new Book("asdasda", "qwe", "asd", 1960),
+            new Book("xzxz", "Jpaa", "Raas", 1813)
+        };
+
+libary.SerializeToJson("\"C:\\\\DZ\\\\users.json\"", books);
+
+
+
+
+
+
+
+
+
+
+libary.DeserializeToJson("C:\\DZ\\users.json");
+Console.WriteLine(libary.Print());
+//Вроде как все работает, но считывает файл через раз( не всегда в консолье его видно, иногда даже 1 раз из 5 может считать) не могу понять в чем ошибка)
