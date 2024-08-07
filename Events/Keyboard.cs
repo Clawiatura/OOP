815423e8b1e2c772746852e7157a61424fb7c1fd
Clawiatura <maksimov19992805@icloud.com>
2024-08-07 14:13:53 +0200

    K

---
+++ Events/Keyboard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace Events
+{
+    internal class Keyboard
+    {
+        public event Action<char> KeyPressed;
+        public  void SimulateKeyPress(char key)
+        {
+            KeyPressed.Invoke(key);
+        }
+        public void PressA() => SimulateKeyPress('A');
+        public void PressB() => SimulateKeyPress('B');
+        public void Press1() => SimulateKeyPress('1');
+    }
+
+}
---
+++ Events/Program.cs
@@ -0,0 +1,27 @@
+﻿using Events;
+
+Keyboard keyboard = new Keyboard();
+keyboard.KeyPressed += OnKeyPressed;
+Console.WriteLine("Нажмите на одну из кнопок на клавиатуре: A,B,1");
+char keyUser = char.Parse(Console.ReadLine());
+if (keyUser == 'A')
+{
+    keyboard.PressA();
+}
+else if (keyUser == 'B')
+{
+    keyboard.PressB();
+}
+else if (keyUser == '1')
+{
+    keyboard.Press1();
+}
+else
+{
+    Console.WriteLine("Такой кнопки нет");
+}
+static void OnKeyPressed(char key)
+{
+    
+    Console.WriteLine($"Клавиша '{key}' нажата.");
+}
