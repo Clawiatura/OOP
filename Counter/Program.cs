846237635e1326d5f8ea2d7a6fb2ba177670eb5a
Clawiatura <maksimov19992805@icloud.com>
2025-01-24 14:26:36 +0200

    s

---
+++ Counter/Program.cs
@@ -0,0 +1,52 @@
+﻿Counter counter = new Counter();
+
+Thread incrementThread = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++) counter.Increment();
+});
+
+Thread decrementThread = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++) counter.Decrement();
+});
+
+incrementThread.Start();
+decrementThread.Start();
+incrementThread.Join();
+decrementThread.Join();
+
+class Counter
+{
+    private int _count;
+    private readonly object _lock = new object();
+
+    public void Increment()
+    {
+        lock (_lock)
+        {
+            _count++;
+            Console.WriteLine($": {_count}");
+        }
+    }
+
+    public void Decrement()
+    {
+        lock (_lock)
+        {
+            _count--;
+            Console.WriteLine($"Декремент: {_count}");
+        }
+    }
+
+    public int GetValue()
+    {
+        lock (_lock)
+        {
+            return _count;
+        }
+    }
+}
+//правильно ли я понял задание?
+
+
+
