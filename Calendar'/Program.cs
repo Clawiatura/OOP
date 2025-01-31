dc093b8067b39ade27c97b7b469b059a2ab466a5
Clawiatura <maksimov19992805@icloud.com>
2025-02-01 01:42:47 +0200

    asd

---
+++ Calendar'/Program.cs
@@ -0,0 +1,57 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO;
+
+FileInfo fileInfo = new FileInfo("Календарь " + DateTime.Now.Year + ".xlsx");
+
+ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+ExcelPackage newBook = new ExcelPackage(fileInfo);
+
+var worksheet = newBook.Workbook.Worksheets.Add("Календарь " + DateTime.Now.Year);
+
+string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+for (int month = 1; month < 12; month++)
+{
+    //название месяца
+    worksheet.Cells[1, month + 1].Value = months[month];
+    worksheet.Cells[1, month + 1].Style.Font.Bold = true;
+
+
+    DateTime firstDay = new DateTime(DateTime.Now.Year, month + 1, 1);
+
+    int daysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, month + 1);
+
+    // дни
+    for (int day = 1; day <= daysInMonth; day++)
+    {
+        DateTime currentDate = new DateTime(DateTime.Now.Year, month + 1, day);
+        int row = day + 1;
+
+
+        worksheet.Cells[row, month + 1].Value = day;
+        worksheet.Cells[row, month + 1].AddComment(currentDate.DayOfWeek.ToString());
+
+        //выходные
+
+        if (currentDate.DayOfWeek == DayOfWeek.Saturday || currentDate.DayOfWeek == DayOfWeek.Sunday)
+        {
+            worksheet.Cells[row, month + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            worksheet.Cells[row, month + 1].Style.Fill.BackgroundColor.SetColor(Color.Violet);
+            worksheet.Cells[row, month + 1].Style.Font.Bold = true;
+        }
+    }
+}
+try
+{
+    newBook.Save();
+    Console.WriteLine("Календарь успешно создан!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка при сохранении файла: {ex.Message}");
+}
---
+++ Kalendar'/Program.cs
@@ -0,0 +1,38 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var fileInfo = new FileInfo("Календарь_2023.xlsx");
+        using (var package = new ExcelPackage(fileInfo))
+        {
+            var worksheet = package.Workbook.Worksheets.Add("Календарь 2023");
+
+            // Заполнение месяцев и дней
+            for (int month = 1; month <= 12; month++)
+            {
+                worksheet.Cells[1, month].Value = new DateTime(2023, month, 1).ToString("MMMM");
+                for (int day = 1; day <= DateTime.DaysInMonth(2023, month); day++)
+                {
+                    var cell = worksheet.Cells[day + 1, month];
+                    cell.Value = day;
+
+                    // Выделение выходных
+                    var date = new DateTime(2023, month, day);
+                    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                        cell.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Yellow);
+                        cell.Style.Font.Bold = true;
+                    }
+                }
+            }
+
+            package.Save();
+        }
+    }
+}
